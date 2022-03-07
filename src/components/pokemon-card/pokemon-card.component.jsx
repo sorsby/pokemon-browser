be84203ed46d9204f09349637277e7e08cc18da9
Matthew Sorsby <matthew.sorsby.iw@cinch.co.uk>
2022-03-07 16:07:09 +0000

    Change some styling.

--- src/components/pokemon-card/pokemon-card.component.jsx
+++ src/components/pokemon-card/pokemon-card.component.jsx
@@ -6,16 +6,20 @@ import "./pokemon-card.styles.scss";
 const PokemonCard = ({ id, name, image, resistant, weaknesses, attacks }) => {
   return (
     <div className="pokemon-card" data-testid="pokemon-card">
-      <h1 data-testid="pokemon-name">{name}</h1>
+      <h1 className="pokemon-name" data-testid="pokemon-name">
+        {name}
+      </h1>
       <img className="pokemon-image" src={image} alt={name}></img>
       <div className="attacks-container">
         <div className="fast">
           <h3 className="fast-title">Fast</h3>
           {attacks.fast.map(({ name, type, damage }) => (
             <div className="fast-attack" key={`${id}-${name}-${damage}`}>
-              <p>{name}</p>
-              <p>{type}</p>
-              <p>{damage}</p>
+              <p className="attack-title">{name}</p>
+              <div className="type-damage-row">
+                <p>{type}</p>
+                <p>{damage}</p>
+              </div>
             </div>
           ))}
         </div>
@@ -23,22 +27,24 @@ const PokemonCard = ({ id, name, image, resistant, weaknesses, attacks }) => {
           <h3 className="special-title">Special</h3>
           {attacks.special.map(({ name, type, damage }) => (
             <div className="special-attack" key={`${id}-${name}-${damage}`}>
-              <p>{name}</p>
-              <p>{type}</p>
-              <p>{damage}</p>
+              <p className="attack-title">{name}</p>
+              <div className="type-damage-row">
+                <p>{type}</p>
+                <p>{damage}</p>
+              </div>
             </div>
           ))}
         </div>
       </div>
       <div className="resistant-weaknesses-container">
         <div className="resistant">
-          <h3>Resistant</h3>
+          <h3 className="resistant-title">Resistant</h3>
           {resistant.map((resist) => (
             <p key={`${id}-${name}-${resist}`}>{resist}</p>
           ))}
         </div>
         <div className="weaknesses">
-          <h3>Weaknesses</h3>
+          <h3 className="weaknesses-title">Weaknesses</h3>
           {weaknesses.map((weak) => (
             <p key={`${id}-${name}-${weak}`}>{weak}</p>
           ))}
